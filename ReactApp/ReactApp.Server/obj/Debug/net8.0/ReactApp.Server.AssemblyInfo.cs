5ff3629e96bc58b842bf0817e8f80d5fd6277476
AfizM <afiz.mahmud@gmail.com>
2024-10-24 00:49:01 +1100

    Delete MyMovieList model

--- ReactApp/ReactApp.Server/obj/Debug/net8.0/ReactApp.Server.AssemblyInfo.cs
+++ ReactApp/ReactApp.Server/obj/Debug/net8.0/ReactApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43f489bee0877cef624563886da543864378914a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0585b9374f1645f85b77a50ab69e461e286836ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
